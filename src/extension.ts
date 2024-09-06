0425438e53aa6aac2c17e0ba4763da9f8bc54e3a
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2024-09-06 18:35:04 +0400

    chore: file tab size changed

--- src/extension.ts
+++ src/extension.ts
@@ -4,23 +4,23 @@ import playSound from 'play-sound';
 const player = playSound();
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "symphocode" is now active!');
+  console.log('Congratulations, your extension "symphocode" is now active!');
 
-	vscode.workspace.onDidChangeTextDocument((evt) => {
-		console.log(evt);
+  vscode.workspace.onDidChangeTextDocument((evt) => {
+    console.log(evt);
 
-		player.play(`${context.extensionPath}/src/audio/sample.mp3`, (err: any) => {
-			if (err) {
-				console.log(err);
-			}
-		});
-	});
+    player.play(`${context.extensionPath}/src/audio/sample.mp3`, (err: any) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  });
 
-	const disposable = vscode.commands.registerCommand('symphocode.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from SymphoCode!');
-	});
+  const disposable = vscode.commands.registerCommand('symphocode.helloWorld', () => {
+    vscode.window.showInformationMessage('Hello World from SymphoCode!');
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate() { }
