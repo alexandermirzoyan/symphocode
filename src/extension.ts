a60ecf758590907b15eff3376d1e8df411836a49
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2024-11-05 20:25:34 +0400

    feat: sound playing experience improved with adding queue system for playing and not playing too frequent key changes NOTE: soundFileNameSet constant is hard coded with 3 newly added audio files

--- src/extension.ts
+++ src/extension.ts
@@ -4,45 +4,64 @@ import playSound from 'play-sound';
 const player = playSound();
 const soundFileNameSet1 = ['A-1.m4a', 'B-1.m4a', 'C-1.m4a', 'D-1.m4a', 'E-1.m4a', 'F-1.m4a', 'G-1.m4a', 'C-01.m4a'];
 const soundFileNameSet2 = ['A-2.m4a', 'B-2.m4a', 'C-2.m4a', 'D-2.m4a', 'E-2.m4a', 'F-2.m4a', 'G-2.m4a', 'A-02.m4a'];
-const soundFileNameSet = [...soundFileNameSet1, ...soundFileNameSet2];
+// const soundFileNameSet = [...soundFileNameSet1, ...soundFileNameSet2];
+const soundFileNameSet = ['piano_note_1_freq_261.wav', 'piano_note_2_freq_329.wav', 'piano_note_3_freq_392.wav'];
 
 const randomizeArray = (input: string[]) => {
   const randomIndex = Math.floor(Math.random() * input.length);
   return input[randomIndex];
 };
 
+const playAllSoundsInAQueue = (fileNames: string[], dirPath: string) => {
+  if (!fileNames.length) {
+    return;
+  }
+
+  const soundFileName = fileNames.shift();
+  player.play(`${dirPath}/${soundFileName}`, (err: any) => {
+    if (err) {
+      return err;
+    }
+
+    console.log('sound played');
+    playAllSoundsInAQueue(fileNames, dirPath);
+  });
+};
+
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "symphocode" is now active!');
 
+  const queue: string[] = [];
+  let timeBetweenKeyChanges = 0;
+
   vscode.workspace.onDidChangeTextDocument((evt) => {
     const changedChar = evt.contentChanges[0]?.text || '';
-    console.log('changedChar :: ', changedChar);
 
     if (!changedChar) {
       return;
     }
 
-    let soundFileName = null;
+    let isIntervalShortBetweenKeyChanges = false;
+    const currentTimeWhileKeyPress = Date.now();
+    const KEY_PRESS_OFFSET_MS = 150;
+    const isNextKeyPressedTooFast = currentTimeWhileKeyPress < timeBetweenKeyChanges + KEY_PRESS_OFFSET_MS;
+    console.log('changedChar :: ', changedChar);
 
-    if (changedChar.includes('\n')) {
-      soundFileName = 'Enter.m4a';
+    // TODO - Avoid if block here. Can be used while declaration
+    if (timeBetweenKeyChanges && isNextKeyPressedTooFast) {
+      isIntervalShortBetweenKeyChanges = true;
     }
 
-    if (changedChar === ' ') {
-      soundFileName = 'Space.m4a';
+    timeBetweenKeyChanges = currentTimeWhileKeyPress;
+
+    if (isIntervalShortBetweenKeyChanges) {
+      console.log('Too short interval');
+      return;
     }
 
-    if (!soundFileName) {
-      soundFileName = randomizeArray(soundFileNameSet);
-    }
-
-    console.log('soundFileName :: ', soundFileName);
-
-    player.play(`${context.extensionPath}/src/audio/${soundFileName}`, (err: any) => {
-      if (err) {
-        console.log(err);
-      }
-    });
+    const soundFileName = randomizeArray(soundFileNameSet);
+    queue.unshift(soundFileName);
+    playAllSoundsInAQueue(queue, `${context.extensionPath}/src/audio`);
   });
 
   const disposable = vscode.commands.registerCommand('symphocode.start', () => {
